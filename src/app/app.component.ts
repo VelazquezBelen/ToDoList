aaa42c77f743b93fb2645a57129416443037007d
VelazquezBelen <velazquezbelenr@gmail.com>
2022-03-06 12:27:28 -0300

    update Manos a la Obra 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto1';
+  tasks = new Array();
+
+  addTask(taskName: string) {
+    this.tasks.unshift({"name": taskName, "done": false});
+  }
+
+  changeTaskState(index: number) {
+    if (this.tasks[index].done == false) 
+      this.tasks[index].done = true;
+    else
+      this.tasks[index].done = false;
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);  
+  }
 }
